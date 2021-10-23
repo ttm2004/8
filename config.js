1e3dd40609045595926af9bd2924b46fa9d92394
Trần Trọng Mạnh <89680872+ttm2004@users.noreply.github.com>
2021-10-23 13:18:17 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-exports.Token = ["TOKEN BOT","TOKEN BOT#2"]///ot Token - Important
-exports.Owner = "Owner ID; //Bot Owner ID - Important
-exports.Developer = "Dev ID; //Bot Dev ID - Important
-exports.Default_Prefix = "!"//Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = "LINK SERVER; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
-exports.Dashboard = "WEBSITE BOT; //Dashboard Your Bot
-exports.Server_ID = "SERVER FOR PREMIUM COMMANDS; // ID Your Server
-exports.mongodb = "MONGODB";
